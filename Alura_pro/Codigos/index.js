72a943511ded513e48d6f54e76dc1da979470f52
Caroline Carnielli <149020867+CarolineDiasCarnielli@users.noreply.github.com>
2024-09-02 08:05:40 -0300

    Add files via upload

---
+++ Alura_pro/Codigos/index.js
@@ -0,0 +1,6 @@
+// Add event listener to images
+document.querySelectorAll('img').forEach(img => {
+    img.addEventListener('click', () => {
+        alert('Professor eu mereço um 10!');
+    });
+});
